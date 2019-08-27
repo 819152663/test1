bda7286e12d3dc55bc3fcc073f9f9d34b1cc2556
tarena <tarena@tarena-PC>
2019-08-27 14:48:08 +0800

    第一次github提交项目

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,14 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+	private Integer id;
+	private String name;
+	private Integer number;
+}
